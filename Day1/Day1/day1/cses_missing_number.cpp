ac2f269aff1fd3f05aa91091cd5a12f558edc3cd
harshit jangu <harshitchoudhary4488@gmail.com>
2025-08-22 16:38:43 +0530

    Day1: solved CSES Missing Number

---
+++ Day1/Day1/day1/cses_missing_number.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int n;
+    cin>>n;
+    vector<int> a(n-1);
+    for(int i=0 ; i<n-1 ; i++){
+        cin>>a[i];
+    }
+    sort(a.begin(), a.end());
+    for(int i=1 ; i<=n ; i++){
+        if(a[i-1]!=i){
+            cout<<i;
+            return 0;
+        }
+    }
+    return 0;
+}

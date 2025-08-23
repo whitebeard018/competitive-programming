3b8b5952ade210a357d820f81aa711e1040cc2a5
harshit jangu <harshitchoudhary4488@gmail.com>
2025-08-23 19:08:37 +0530

    day2: solved cses two knights

--- DAY2/cses_two_knights.cpp
+++ DAY2/cses_two_knights.cpp
@@ -5,13 +5,15 @@ int main() {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
 
-    long long n;
+    long long  int n;
     if (!(cin >> n)) return 0;
 
     for (long long k = 1; k <= n; ++k) {
         long long total = (k*k) * (k*k - 1) / 2;   
         long long bad   = 4 * (k - 1) * (k - 2);   
         cout << (total - bad) << "\n";
-    }
+    } 
     return 0;
 }
+
+

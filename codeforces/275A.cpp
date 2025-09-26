4935ce88e0bbcfa822be04a9d302d6c3f2340a0f
harshit jangu <harshitchoudhary4488@gmail.com>
2025-09-26 22:28:49 +0530

    incomplete

---
+++ codeforces/275A.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+int  main(){
+    int a[3][3];
+    for(int i=0;i<3;i++){
+        for(int j=0;j<3;j++){
+            cin>>a[i][j];
+        }
+        cout<<endl;
+    }
+    for(int i=0;i<3;i++)
+    {
+        for(int j=0; j<3 ; j++){
+         if(j==0){
+            a[i]
+         }
+    }
+        cout<<endl;
+    }
+}

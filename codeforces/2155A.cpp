3e63ed500fbaacd23481f77730bb2029373c19e1
harshit jangu <harshitchoudhary4488@gmail.com>
2025-10-06 19:01:24 +0530

    easy-mid

---
+++ codeforces/2155A.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+        cout << 2 * n - 2 << "\n";
+    }
+}
